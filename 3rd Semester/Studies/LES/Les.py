e4972b6df93808b20744dc112571a992d119cad4
Lucas Nascimento <159586908+lukasnascky@users.noreply.github.com>
2025-08-26 22:37:27 -0300

    Update Les.py

--- 3rd Semester/Studies/LES/Les.py
+++ 3rd Semester/Studies/LES/Les.py
@@ -5,14 +5,31 @@ class Les:
         self.quant = 0
 
     def inserir_fim(self, valor):
-        if self.quant != self.tam_maximo:
+        if not self.esta_cheia():
             self.vetor[self.quant] = valor
             self.quant += 1
             return True
         return False
 
     def remover_fim(self):
-        if self.quant != 0:
+        if not self.esta_vazia():
+            self.quant -= 1
+            return True
+        return False
+
+    def inserir_inicio(self, valor):
+        if not self.esta_cheia():
+            for i in range(self.quant, 0, -1):
+                self.vetor[i] = self.vetor[i - 1]
+            self.vetor[0] = valor
+            self.quant += 1
+            return True
+        return False
+
+    def remover_inicio(self):
+        if not self.esta_vazia():
+            for i in range(self.quant-1):
+                self.vetor[i] = self.vetor[i + 1]
             self.quant -= 1
             return True
         return False
@@ -31,3 +48,16 @@ class Les:
         if self.quant == 0:
             return True
         return False
+    
+    def ver_primeiro(self):
+        return self.vetor[0]
+    
+    def ver_ultimo(self):
+        return self.vetor[self.quant - 1]
+    
+    def tamanho_atual(self):
+        return self.quant
+    
+    def capacidade(self):
+        return self.tam_maximo
+    
